17fedf767a1e0f3a0ecdf6753db5f3ac07ea26bb
Daniel-Slabaugh <danielslabaugh@gmail.com>
2017-09-02 12:09:04 -0500

    changed db path

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.DATABASE_URL = process.env.MONGODB_URI ||
+exports.DATABASE_URL = process.env.MONGOLAB_WHITE_URI ||
                       'mongodb://localhost/blog-app';
 exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
                       'mongodb://localhost/test-blog-app');
